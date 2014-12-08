22e66fbf855635718e57bdd7e2892eef09158631
Jayin Ton <tonjayin@gmail.com>
2014-12-08 16:21:55 +0800

    Initial Commit

---
+++ OCBasic1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCBasic1
+//
+//  Created by Jayin on 14/12/8.
+//  Copyright (c) 2014年 Jayin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
